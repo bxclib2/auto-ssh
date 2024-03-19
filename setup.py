bdad2806d1b4d763e4414a5c182bc22e33793881
bxclib2 <47888430+bxclib2@users.noreply.github.com>
2024-03-19 10:14:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autossh',
-    version='0.0.1.1',
+    version='0.0.1.2',
     license='MIT',
     author='Gavin Bao',
     author_email='xingce.bao@gmail.com',
@@ -10,6 +10,11 @@ setup(
     install_requires=[
         'sshtunnel',
     ],
+    entry_points={ 
+        'console_scripts': [
+            'autossh-py=autossh.autossh_py:main',  
+        ],
+    },
     description='A simple auto reconnect ssh tunnel client',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown'
